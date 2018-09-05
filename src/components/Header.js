9c5d8598adbaaa6693588dd1fb667df7531afa61
jwaltrip <jake.waltrip@gmail.com>
2018-09-05 15:51:11 -0500

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = (props) => {
+  return (
+    <div className="header">
+      <div className="header-item">Bio</div>
+      <div className="header-item">Projects</div>
+      <div className="header-item">Work History</div>
+      <div className="header-item">Contact Me</div>
+    </div>
+  );
+};
