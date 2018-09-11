67ee347c5d3f4b44a7e7430fd78391f39b6a38d2
jwaltrip <jake.waltrip@gmail.com>
2018-09-11 16:01:48 -0500

    inital images working in projects comp

--- src/App.js
+++ src/App.js
@@ -20,16 +20,23 @@ class App extends Component {
           <div className="container">
             <Project
               title="React Todo List"
-              desc="A simple todo list written in React."
-            />
-            <Project
-              title="React Scoreboard"
-              desc="A scoreboard written in React to keep track of winners of: card games, board games, etc."
+              desc="A simple todo list written in React - fully styled by myself."
+              demoLink="https://jwaltrip.github.io/react-todo-list/"
+              imgSrc="https://i.imgur.com/IJ4XNOc.png"
+              limitImgHeight={true}
             />
             <Project
               title="EDI Web Portal"
               desc="A web portal built to convert inbound EDI 850 purchase order data and display it nicely formatted."
             />
+            <Project
+              title="React Scoreboard"
+              desc="A scoreboard written in React to keep track of winners of: card games, board games, etc."
+              demoLink="https://jwaltrip.github.io/react-scoreboard/"
+              imgSrc="https://i.imgur.com/WC1BsX1.png"
+              limitImgHeight={false}
+            />
+
 
           </div>
         </section>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
+import GithubIcon from "./GithubIcon";
 
 const Project = (props) => {
+  let imageWidth = '100%';
+  let imageHeight = '';
+  if (props.limitImgHeight) {
+    imageHeight = '100%';
+    imageWidth = '';
+  }
+
   return (
     <div className="project">
       <div className="project-img">
-        img goes here
+        <img src={props.imgSrc} width={imageWidth} height={imageHeight} />
       </div>
       <div className="project-footer">
-        <div className="project-title">{props.title}</div>
-        <div className="project-desc">{props.desc}</div>
+        <div className="footer-container">
+          <div className="project-title">{props.title}</div>
+          <div className="project-desc">{props.desc}</div>
+          <div className="project-chips">
+            <Chip
+              avatar={<Avatar src="https://simpleicons.org/icons/github.svg"/>}
+              label="Demo"
+              component="a"
+              href={props.demoLink}
+              target="_blank"
+              clickable
+            />
+            <Chip
+              avatar={<Avatar src="https://simpleicons.org/icons/github.svg"/>}
+              label="GitHub Source"
+              component="a"
+              href={props.githubSrcLink}
+              clickable
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
