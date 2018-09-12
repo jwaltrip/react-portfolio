b1e767ed031389a97823f1270ffc58a80d3437bd
jwaltrip <jake.waltrip@gmail.com>
2018-09-12 16:30:55 -0500

    initial toggle of contact form

--- src/App.js
+++ src/App.js
@@ -14,11 +14,15 @@ class App extends Component {
     showContactForm: false
   };
 
+  toggleContactForm = () => {
+    this.setState({ showContactForm: !this.state.showContactForm });
+  };
+
   render() {
     // add css class to toggle show/hide of contact form
     let showContactFormClass = '';
-    if (this.state.showContactForm) {
-      showContactFormClass = ' show-contact-form';
+    if (!this.state.showContactForm) {
+      showContactFormClass = ' hide';
     }
 
     return (
@@ -27,18 +31,18 @@ class App extends Component {
         <ProjectsSection />
 
         <section className="contact-form-section">
-          <h2 className="contact-header-text">Contact Me</h2>
+          <div className="contact-header-text" onClick={this.toggleContactForm}>Contact Me</div>
 
           <div className={"contact-form-container" + showContactFormClass}>
-            <div className="contact-form-header">
+            <div className={"contact-form-header" + showContactFormClass}>
               Let's Get Together!
             </div>
-            <div className="contact-sub-text">
+            <div className={"contact-sub-text" + showContactFormClass}>
               Would you like to chat? Grab some coffee? Collab on a project?<br/>
               So would I!
             </div>
-            <div className="contact-form">
-              <form className="form" id="contact-form">
+            <div className={"contact-form" + showContactFormClass}>
+              <form className={"form" + showContactFormClass} id="contact-form">
                 <input type="text" placeholder="Your Name" />
                 <input type="text" placeholder="Your Email" />
                 <input type="text" placeholder="Your Location" />
