e83f611fd97b93fcb16c1efb7edfa2690e17b826
jwaltrip <jake.waltrip@gmail.com>
2018-10-03 18:09:07 -0500

    added reddit image and fixed scoreboard image

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -20,9 +20,11 @@ const ProjectGrid = (props) => {
           <h2>Projects</h2>
         </div>
       </Hidden>
-      {/*<Visible xs sm md>*/}
-        {/*<h2>Projects</h2>*/}
-      {/*</Visible>*/}
+      <Hidden xs sm md>
+        <div className="desktop-projects-header">
+          <h2>Projects</h2>
+        </div>
+      </Hidden>
 
       <Row>
         <Col md={col.md} xl={col.lg} >
@@ -33,6 +35,17 @@ const ProjectGrid = (props) => {
             githubSrcLink="https://github.com/jwaltrip/react-todo-list"
             imgSrc="https://i.imgur.com/IJ4XNOc.png"
             enlargedImg="https://i.imgur.com/IJ4XNOc.png"
+          />
+        </Col>
+        <Col md={col.md} xl={col.lg} >
+          <ProjectBootstrap
+            title="React Reddit Viewer"
+            desc="A simple React view that fetches Reddit's /r/all feed, and displays the feed with simple pagination."
+            demoLink="https://jwaltrip.github.io/react-reddit-viewer"
+            imgSrc="https://i.imgur.com/IgWUJjI.jpg"
+            enlargedImg="https://i.imgur.com/IgWUJjI.jpg"
+            limitImgHeight={false}
+            githubSrcLink="https://github.com/jwaltrip/react-reddit-viewer"
           />
         </Col>
         <Col md={col.md} xl={col.lg} >
@@ -48,21 +61,10 @@ const ProjectGrid = (props) => {
             title="React Scoreboard"
             desc="A scoreboard written in React to keep track of winners of: card games, board games, etc."
             demoLink="https://jwaltrip.github.io/react-scoreboard/"
-            imgSrc="https://i.imgur.com/drH5uNA.png"
-            enlargedImg="https://i.imgur.com/drH5uNA.png"
+            imgSrc="https://i.imgur.com/tUGtg6Q.png"
+            enlargedImg="https://i.imgur.com/tUGtg6Q.png"
             limitImgHeight={false}
             githubSrcLink="https://github.com/jwaltrip/react-scoreboard"
-          />
-        </Col>
-        <Col md={col.md} xl={col.lg} >
-          <ProjectBootstrap
-            title="React Reddit Viewer"
-            desc="A simple React view that fetches Reddit's /r/all feed, and displays the feed with simple pagination."
-            demoLink="https://jwaltrip.github.io/react-reddit-viewer"
-            imgSrc=""
-            enlargedImg=""
-            limitImgHeight={false}
-            githubSrcLink="https://github.com/jwaltrip/react-reddit-viewer"
           />
         </Col>
       </Row>
